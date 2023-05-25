51046dd8e3c8a7ec2c96a71b54374dcfed6144df
Adnan <aadnansevinc@gmail.com>
2023-05-25 21:56:37 +0200

    Assignment 5/25/2023

---
+++ Quarto and MD 3/Assignment.R
@@ -0,0 +1,11 @@
+# Rendering
+#install.packages("quarto")
+library(quarto)
+
+for ( s in 1:8){
+  quarto_render("Assignment.qmd", execute_params = list(
+    data = s), output_file = paste0("Report-", s, ".html"))
+  
+}
+
+
